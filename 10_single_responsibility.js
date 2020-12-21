fed23b6068ced88c9cd144367906250bc5f70607
Alexis Crespo <alexis-2cab@hotmail.com>
2020-12-21 11:18:32 -0500

    SOLID principles

---
+++ 10_single_responsibility.js
@@ -0,0 +1,14 @@
+/* SOLID
+  Single Responsibility: A class should have one and only one reason to change,
+                         meaning that a class should have only one job.
+
+  Open closed: Classes are open to adition but close to modifications.
+
+  Liskov substitution: Every sublcass can substitute his parent class without
+                       affecting the result.
+
+  Interface Segregation: Sublcasses should be able to use all the parent methods
+                         and properties, if not, break in multiple components.
+
+  Dependency inversion: Depend on abstractions instead of the low level API.
+*/
